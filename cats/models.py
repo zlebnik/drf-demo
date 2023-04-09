5b84b246da54a3b58a9ef68a1c3a242b51cd2afc
Gleb Kovalev <kovalev@mindbox.cloud>
2023-04-09 09:11:07 +0300

    feat: inital models

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Cat(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=1024)
